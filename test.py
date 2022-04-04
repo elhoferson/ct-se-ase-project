73485b2f3189a481ffbe92e502cf18fdf482d4af
kevwil <kewiltschnig@edu.aau.at>
2022-04-04 10:52:33 +0200

    test

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ dataset = 'data/MSR_data_cleaned.csv'
 chunksize = 10 ** 5
 count = 0
 total = 0
-with pd.read_csv(dataset, chunksize=chunksize) as reader:
+with pd.read_csv(dataset, chunksize=chunksize, low_memory=False) as reader:
     for chunk in reader:
         vulCol = chunk['vul']
         print(vulCol.value_counts())
